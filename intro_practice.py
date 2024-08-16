b245d937bc98064bdecc9ce30ebdfd1774f8a873
Simpson <Matthew.Simpson@stonybrookmedicine.edu>
2024-08-16 15:21:13 -0400

    Modified script

--- intro_practice.py
+++ intro_practice.py
@@ -1,1 +1,31 @@
 import pandas as pd
+import numpy as np
+x = y = z = "python"
+print (x)
+print (y) 
+print (z)
+
+in [8] : b = (x) + (y) + (z)
+b = "python"
+print ( x + y + z)    
+
+in [12] : a, b, c = 5, 3.2, "hello" 
+print (a)  
+print (b) 
+print (c)
+
+in [17] : color_a = green
+in [18] : color_b = yellow
+in [19] ; color_c = blue
+in [20] : color_c = color_a + color_b
+
+print (color_a , color_b, color_c)
+print  (color_c = color_a + color_b) 'blue
+blue = mix_yellow_green (blue)
+print("mix_yellow_green:" blue)   
+
+
+in [22] : x = y == z
+print (x)
+
+
